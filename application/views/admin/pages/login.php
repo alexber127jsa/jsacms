d1c0bce04c5beaa231ab0b3d30cea701c146a008
alexber127jsa <alexber127@yandex.ru>
2014-08-11 20:14:59 +0600

    Добавление формы авторизации

--- application/views/admin/pages/login.php
+++ application/views/admin/pages/login.php
@@ -1,6 +1,10 @@
-<div>
-    <ul>
-        <li>321321</li>
-        <li><a href="#">321321</a></li>
-    </ul>
+<div class="center">
+    <div class="center_head">JSACms <span>(ver. 0.1.0)</span></div>
+    <div class="center_content">
+        <label>Логин</label>
+        <input type="text" name="login" />
+        <label>Пароль</label>
+        <input type="password" name="password" />
+        <div class="submit_button">Войти</div>
+    </div>
 </div>
